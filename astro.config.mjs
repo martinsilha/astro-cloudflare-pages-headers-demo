416c997ad032d1a37ea575aeaa61245680a84a74
Martin Silha <martysilha@gmail.com>
2025-02-13 02:19:38 -0600

    feat: refactor headers configuration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-import { astroCloudflarePagesHeaders } from '../index'
+import astroCloudflarePagesHeaders from '../src/integration'
+
+export const headers = {
+  'x-hello-world': 'Hello, World!',
+  // 'cache-control': 'public, max-age=0, must-revalidate',
+  // 'x-frame-options': 'DENY',
+  // 'x-content-type-options': 'nosniff',
+  // 'x-xss-protection': '1; mode=block',
+  // 'referrer-policy': 'same-origin',
+  // 'feature-policy': "geolocation 'self'; microphone 'self'; camera 'self'",
+  // 'content-security-policy': `default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content;`,
+  // 'strict-transport-security': 'max-age=31536000; includeSubDomains',
+  // 'permissions-policy': "geolocation=(self), microphone=(self), camera=(self)"
+}
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    headers: {
-      'cache-control': 'public, max-age=0, must-revalidate',
-      'x-frame-options': 'DENY',
-      'x-content-type-options': 'nosniff',
-      'x-xss-protection': '1; mode=block',
-      'referrer-policy': 'same-origin',
-      'feature-policy': "geolocation 'self'; microphone 'self'; camera 'self'",
-      'content-security-policy': `default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; media-src 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self'; upgrade-insecure-requests; block-all-mixed-content;`,
-      'strict-transport-security': 'max-age=31536000; includeSubDomains',
-      'permissions-policy': "geolocation=(self), microphone=(self), camera=(self)"
-    },
+    headers
   },
   integrations: [astroCloudflarePagesHeaders()],
 })
